ea20c401e9187ecd57f954e46dd8a1d68a89072d
TeknoTF2 <114828609+TeknoTF2@users.noreply.github.com>
2025-02-04 00:28:26 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Configure for static export
-  output: 'export',
-  images: {
-    unoptimized: true,
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
   },
 }
 
