572f8b83f2fc581e451649aeccbd114d8b47645a
TeknoTF2 <114828609+TeknoTF2@users.noreply.github.com>
2025-02-03 23:59:12 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Disable server-side rendering for the entire app
+  // since we're using browser APIs
+  typescript: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
