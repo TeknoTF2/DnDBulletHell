c4960b377359499dc2cc13ac4777fd6c26465fce
TeknoTF2 <114828609+TeknoTF2@users.noreply.github.com>
2025-02-03 23:52:09 -0800

    Update index.jsx

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Card from '../ui/Card';
+import Card from '../../components/ui/Card';
 import Grid from './Grid';
 import BackgroundSettings from './BackgroundSettings';
 import PlayerControls from './PlayerControls';
