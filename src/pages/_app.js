aac1abac493e7ff1df845ec7bfadc62cc4656f30
TeknoTF2 <114828609+TeknoTF2@users.noreply.github.com>
2025-02-04 09:09:33 -0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,29 @@
 import '../styles/globals.css';
 import { SocketProvider } from '../context/SocketContext';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Add visible console logging on the client
+    console.log('App mounted');
+    
+    // Log any unhandled errors
+    window.onerror = function(msg, url, lineNo, columnNo, error) {
+      console.error('Window Error:', { msg, url, lineNo, columnNo, error });
+      return false;
+    };
+
+    window.onunhandledrejection = function(event) {
+      console.error('Unhandled Promise Rejection:', event.reason);
+    };
+  }, []);
+
   return (
-    <SocketProvider>
-      <Component {...pageProps} />
-    </SocketProvider>
+    <div style={{ minHeight: '100vh' }}>
+      <SocketProvider>
+        <Component {...pageProps} />
+      </SocketProvider>
+    </div>
   );
 }
 
